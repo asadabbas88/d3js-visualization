210f4af92a6852ac0346fcd14a0bdc6e6e8ec694
ali_webxity <ali.faizan@webxity.com>
2015-05-07 20:51:38 +0500

    removing collection id from constants

--- client/app/constants.js
+++ client/app/constants.js
@@ -5,6 +5,5 @@ angular.module('constants', []).constant('CONFIG', {
   SEARCH_BASE_URL: 'http://119.81.106.34/searchblox/servlet/SearchServlet',
   WORDCLOUD_FACET_SIZE: 100,
   QUESTIONS: ['Q4', 'Q5'],
-  COLLECTION_ID: 2,
   PAGE_SIZE: 250
 });
