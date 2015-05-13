33c37b1289c8e9dfb294761b3da593b8389cb8c1
ali_webxity <ali.faizan@webxity.com>
2015-05-14 00:31:17 +0500

    data tables WIP

--- client/app/app.js
+++ client/app/app.js
@@ -12,7 +12,24 @@ angular.module('Angular-visualization-module', [
   'ngTable',
   'search.service',
   'constants',
-  'underscore'
+  'underscore',
+  'ui.bootstrap',             // AngularJS native directives for Bootstrap
+  'angular-flot',             // Flot chart
+  'angles',                   // Chart.js
+  'angular-peity',            // Peity (small) charts
+  'cgNotify',                 // Angular notify
+  'angles',                   // Angular ChartJS
+  'ngAnimate',                // Angular animations
+  'ui.map',                   // Ui Map for Google maps
+  'ui.calendar',              // UI Calendar
+  'summernote',               // Summernote plugin
+  'ngGrid',                   // Angular ng Grid
+  'ui.tree',                  // Angular ui Tree
+  'bm.bsTour',                // Angular bootstrap tour
+  'datatables',               // Angular datatables plugin
+  'xeditable',                // Angular-xeditable
+  'ui.select',                // AngularJS ui-select
+  'ui.sortable'               // AngularJS ui-sortable
 ])
   .config(function ($httpProvider, $stateProvider, $urlRouterProvider, $locationProvider) {
     $httpProvider.defaults.useXDomain = true;
