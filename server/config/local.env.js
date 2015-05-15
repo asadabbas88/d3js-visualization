2da36bfc0fc6e867fd39ef5d76b09cb925b67313
ali_webxity <ali.faizan@webxity.com>
2015-05-15 22:39:09 +0500

    storm path application and cosmetic changes

--- server/config/local.env.js
+++ server/config/local.env.js
@@ -13,5 +13,5 @@ module.exports = {
   DEBUG: '',
   STORMPATH_API_KEY_ID: '62DXM2KQZT51XPSUD1W9I7WZH',
   STORMPATH_API_KEY_SECRET: 'CuCqd1myqVbUBkyzg70Qpv4qwTL/fsqY+MUqmfh0GN0',
-  STORMPATH_APP_HREF: 'https://api.stormpath.com/v1/applications/2tHDuNzRqL2CbJdBDPupZQ'
+  STORMPATH_APP_HREF: 'https://api.stormpath.com/v1/applications/1o0G22UUCLY9RnKpxwC9qE'
 };
