5fc09d4d24fe93b33054268ac3bd154d6f41857a
muhammad ahsan <ahsan@webxity.com>
2015-05-25 20:03:12 +0500

    working on logo dynamic call and some changes on css

--- client/app/login/login.controller.js
+++ client/app/login/login.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('Angular-visualization-module')
-    .controller('LoginCtrl', function ($scope, $auth) {
+    .controller('LoginCtrl', function ($scope, $auth, $http,$user) {
 
       $scope.formModel = {
         username: '',
@@ -12,6 +12,32 @@ angular.module('Angular-visualization-module')
         $scope.posting = true;
         $scope.error = null;
         $auth.authenticate($scope.formModel)
+            .then(function(){
+                sessionStorage.setItem(
+                    'isViewer',
+                    false
+                );
+                for(var i = 0; i < $user.currentUser.groups.length; i++) {
+                    if($user.currentUser.groups[i].name == 'Viewer') {
+                        $http.get('/api/user/directory?href=' + $user.currentUser.directory.href).success(function (directory) {
+                            sessionStorage.setItem(
+                                'collectionId',
+                                directory.customData.collectionId
+                            );
+                            sessionStorage.setItem(
+                                'logoUrl',
+                                directory.customData.logoUrl
+                            );
+                            sessionStorage.setItem(
+                                'isViewer',
+                                true
+                            );
+                        });
+                        $scope.isViewer = true;
+                        break;
+                    }
+                }
+             })
             .catch(function(response){
               $scope.posting = false;
               $scope.error = response.data.errorMessage;
